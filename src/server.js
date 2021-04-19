d8fb87552dfc2865fb7d58b41bf7d39108a6097b
niclasgardsuu <niclasgards@hotmail.com>
2021-04-19 11:34:43 +0200

    oopsie

--- src/server.js
+++ src/server.js
@@ -1,147 +1,4 @@
-<<<<<<< HEAD
-function main() {
-    var app = require('express')();
-    var nodemailer = require('nodemailer');
-    var bigInt = require('big-integer');
-    var aes256 = require('aes256');
-    
-    const Database = require('better-sqlite3');
-    const db = new Database('database.db', { verbose: console.log });
 
-    var clients = [];
-
-    /** 
-     * CORS is a mechanism which restricts us from hosting both the client and the server.
-     * The package cors allows us the bypass this
-     * */ 
-    var cors = require('cors');
-    app.use(cors());
-
-    /// Creates an HTTP server using ExpressJS
-    var http = require('http').createServer(app);
-    const PORT = 8080;
-    /// The cors: ... is also required to bypass the restriction stated above
-    var client = require('socket.io')(http, {cors: {origin:"*"}});
-
-    /// Starts listening on the chosen port
-    http.listen(PORT, () => {
-        console.log(`listening on *:${PORT}`);
-    });
-
-    /**
-     * @summary Determines the behaviour for when 
-     * a client connects to our socket.
-     */
-    client.on('connection', (socket) => {
-        console.log("new client connected");
-        socket.emit('connection');
-
-        /**
-         * @summary When the socket receives a register signal,
-         * the username, password and email is checked and a
-         * corresponding success/fail message is sent
-         */
-        socket.on('register', (username, password, email) => {
-            if(clientRegister(username, password, email, db)) socket.emit('registerSuccess');
-            else socket.emit('registerFailure');
-        });
-
-        /**
-         * @summary When the socket receives a login signal,
-         * the username and password is checked and a
-         * corresponding success/fail message is sent
-         */
-        socket.on('login', (username, password) => {
-            if(clientLogin(username, password, db) === "valid") socket.emit('loginSuccess');
-            else if(clientLogin(username, password, db) === "root") socket.emit('loginRoot');
-            else socket.emit('loginFailure');
-        });
-
-        /**
-         * @summary When the socket receives a resetPass signal,
-         * the mail is checked in the database and a corresponding 
-         * success/fail message is sent. If successful, a code is
-         * sent to the mail
-         */
-        socket.on('resetPass', (email) => {
-            if(checkMail(email, db)) { 
-                var code = generateCode(8);
-                insertCode(code, email, db);
-                sendMail(code, email, nodemailer);
-                socket.emit('emailSuccess');
-            } else {
-                socket.emit('emailFailure');       
-            }
-        })
-
-        /**
-         * @summary When the socket receives a submitCode signal,
-         * the code and email is checked against the database and
-         * a corresponding success/fail message is sent.
-         */
-        socket.on('submitCode', (code, email) => {
-            if(checkCode(code, email, db)) socket.emit('codeSuccess');
-            else socket.emit('codeFailure');
-        })
-
-        /**
-         * @summary When the socket receives an updatePass signal,
-         * the users password is updated with the received password
-         */
-        socket.on('updatePass', (email, password) => {
-            if(updatePassword(password, email, db)) socket.emit("updatePassSuccess");
-            else socket.emit("updatePassFailure");
-        });
-
-        socket.on('addQuestion', (question, answers) => {
-            if(!addQuestion(question, answers, db)) socket.emit("questionFailure");
-        })
-        
-
-        socket.on('startKeyExchange', () => {
-            var server_private_key = bigInt(4201337); //TODO bättre keys här. randomizeade, helst 256 bit nummer läste jag på google
-            var g = bigInt(2579);
-            var p = bigInt(5159);
-            var server_public_key = g.modPow(server_private_key,p);
-            client.emit('serverPublic', Number(server_public_key), Number(g), Number(p));
-        });
-        
-        socket.on('clientPublic',(client_public_key) => {
-            var server_private_key = bigInt(4201337); //TODO bättre keys här. randomizeade, helst 256 bit nummer läste jag på google
-            var g = bigInt(2579);
-            var p = bigInt(5159);
-            client_public_key = bigInt(client_public_key);
-            
-            var shared_key = client_public_key.modPow(server_private_key,p);
-            console.log("the established key: " + shared_key);
-
-            clients.push({
-                id: socket.id,
-                key: shared_key
-            });
-        });
-
-        socket.on('testEncrypt', (encryptedMsg) => {
-            var sharedKey;
-            for(cli of clients) {
-                if(cli.id == socket.id) sharedKey = cli.key;
-            }
-            console.log(sharedKey.toString());
-            var msg = aes256.decrypt(sharedKey.toString(), encryptedMsg);
-
-            console.log(msg);
-        })
-
-        /**
-         * @summary emits the current time left to every connected
-         * client every second
-         */
-        setInterval(() =>{
-            socket.emit("timeLeft", stringifySeconds(counter))
-        }, 1000);
-    });
-}
-=======
 
 let users = [];
 var backend = require('./backend')
@@ -151,7 +8,6 @@ var bigInt = require("big-integer");
 
 const Database = require("better-sqlite3");
 const db = new Database("database.db", { verbose: console.log });
->>>>>>> origin
 
 /**
  * CORS is a mechanism which restricts us from hosting both the client and the server.
@@ -298,26 +154,40 @@ server.on("connection", (socket) => {
     else socket.emit("returnUserFailure");
   });
 
-  socket.on("start-key-exchange", () => {
+  socket.on('startKeyExchange', () => {
     var server_private_key = bigInt(4201337); //TODO bättre keys här. randomizeade, helst 256 bit nummer läste jag på google
     var g = bigInt(2579);
     var p = bigInt(5159);
-    var server_public_key = g.modPow(server_private_key, p);
-    client.emit(
-      "server-public",
-      Number(server_public_key),
-      Number(g),
-      Number(p)
-    );
-  });
+    var server_public_key = g.modPow(server_private_key,p);
+    client.emit('serverPublic', Number(server_public_key), Number(g), Number(p));
+});
 
-  socket.on("client-public", (client_public_key) => {
+socket.on('clientPublic',(client_public_key) => {
     var server_private_key = bigInt(4201337); //TODO bättre keys här. randomizeade, helst 256 bit nummer läste jag på google
     var g = bigInt(2579);
     var p = bigInt(5159);
     client_public_key = bigInt(client_public_key);
-    var shared_key = client_public_key.modPow(server_private_key, p);
-  });
+    
+    var shared_key = client_public_key.modPow(server_private_key,p);
+    console.log("the established key: " + shared_key);
+
+    clients.push({
+        id: socket.id,
+        key: shared_key
+    });
+});
+
+socket.on('testEncrypt', (encryptedMsg) => {
+    var sharedKey;
+    for(cli of clients) {
+        if(cli.id == socket.id) sharedKey = cli.key;
+    }
+    console.log(sharedKey.toString());
+    var msg = aes256.decrypt(sharedKey.toString(), encryptedMsg);
+
+    console.log(msg);
+})
+
 
   /**
    * @summary This code is ran every 1000ms and counts down
